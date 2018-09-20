f8d2475f5b3e04f86235849853a06cf23df9eb5f
DylanHedges <d.hedges1@hotmail.co.uk>
2018-09-20 08:02:58 -0400

    Connected React and Redux store

--- react-redux-todos/src/index.js
+++ react-redux-todos/src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+//Import the create store functionality
+import {createStore} from "redux";
+//Import the rootReducer
+import rootReducer from "./rootReducer";
+//Connects React app with the Redux store - allows React components to dispatch actions, without only the store can dispatch actions
+import {Provider} from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//Create the store using the root reducer; "window..." - copied from redux devtools (zalmoxisus)
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+//<Provider store={store}> - wrap app using provider tags, connects React app with Redux store
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
