8cf8ae17f37948d9814e2ce9b097641df50004d5
DylanHedges <d.hedges1@hotmail.co.uk>
2018-11-05 08:31:51 -0500

    Added mlab URL - MongoDB

--- react-redux-todos-backend/models/todo.js
+++ react-redux-todos-backend/models/todo.js
@@ -1,11 +1,14 @@
+const {mongoDbUrl} = require("../config");
+
 //Brings in Mongoose
 const mongoose = require("mongoose");
 //Connects to Mongoose
-mongoose.connect("mongodb://localhost/todos-backend", { useNewUrlParser: true });
+mongoose.connect(mongoDbUrl);
 //Log Mongo queries for debugging
 mongoose.set("debug", true);
 //Use ES2015 promises
 mongoose.Promise = Promise;
+
 
 //Schema
 const todoSchema = new mongoose.Schema({
