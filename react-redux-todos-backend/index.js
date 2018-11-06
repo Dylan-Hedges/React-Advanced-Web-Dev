482b00f4954f3eb513fe6b20aea49794fb119df9
DylanHedges <d.hedges1@hotmail.co.uk>
2018-11-06 07:58:25 -0500

    Added comments for back end

--- react-redux-todos-backend/index.js
+++ react-redux-todos-backend/index.js
@@ -1,21 +1,26 @@
 const express = require("express");
 const app = express();
+//Allows us to make requests from localhost:3001 (backend) to localhost:3000 (frontend create-react-app)
 const cors = require("cors");
+//Dels with server-side logging
 const morgan = require("morgan");
+//Allows us to grab req.body for POST requests
 const bodyParser = require("body-parser");
 const todoRoutes = require("./routes/todos");
 
+//Use morgan, bodyparser and cors in our app
 app.use(morgan("tiny"));
 app.use(bodyParser.json());
 app.use(cors());
+//Pre-fix all routes with the string below
 app.use("/api/todos", todoRoutes);
 
+//Error handeling logic
 app.use(function(req, res, next){
   let err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
-
 app.use(function(err, req, res, next){
   res.status(err.status || 500);
   res.send({
@@ -24,6 +29,7 @@ app.use(function(err, req, res, next){
   });
 });
 
+//App listens on port 3001
 app.listen(3001, function(){
   console.log("Server starting on port 3001")
 })
--- react-redux-todos-backend/routes/todos.js
+++ react-redux-todos-backend/routes/todos.js
@@ -1,22 +1,23 @@
 const express = require("express");
 const router = express.Router();
+//Bring in Mongoose model
 const Todo = require("../models/todo");
 
-//GET all todos route
+//GET all todos route; ".find" = mongoose method
 router.get("/", function(req, res, next){
   Todo.find({})
     .then(todos => res.send(todos))
     .catch(err => next(err));
 });
 
-//CREATE todo route
+//CREATE todo route; ".create" = mongoose method
 router.post("/", function(req, res, next){
   Todo.create(req.body)
     .then(todo => res.status(201).send(todo))
     .catch(err => next(err));
 });
 
-//DELETE todo route
+//DELETE todo route; ".findByIdAndRemove" = mongoose method
 router.delete("/:id", function(req, res, next){
   Todo.findByIdAndRemove(req.params.id)
     .then(todo => res.send(todo))
